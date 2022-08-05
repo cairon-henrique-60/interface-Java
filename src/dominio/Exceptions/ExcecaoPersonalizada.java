0a44ddf01e18e96aac2c115eac37205c43766b5b
cairon-henrique-60 <caironhenrique60@gmail.com>
2022-08-05 02:14:12 -0300

    Criando repo

--- src/dominio/Exceptions/ExcecaoPersonalizada.java
+++
@@ -1,9 +0,0 @@
-package dominio.Exceptions;
-
-public class ExcecaoPersonalizada extends Exception{
-	private static final long serialVersionUID = 1L;
-
-	public ExcecaoPersonalizada(String msg) {
-		super(msg);
-	}
-}
--- src/servicos/ServicoDePagamentoOnline.java
+++
@@ -1,7 +0,0 @@
-package servicos;
-
-public interface ServicoDePagamentoOnline {
-	
-	Double taxaDePagamento(double valor);
-	Double jurosPorMes(double valor, int meses);
-}
